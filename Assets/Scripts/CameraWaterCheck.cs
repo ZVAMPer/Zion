d7da21ab9c4737c8984061d1a3f6403200fb9ad3
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-01-24 18:46:26 -0800

    disable cwc

--- Assets/Scripts/CameraWaterCheck.cs
+++ Assets/Scripts/CameraWaterCheck.cs
@@ -6,28 +6,28 @@ public class CameraWaterCheck : MonoBehaviour {
     
     private List<Collider> triggers = new List<Collider> ();
 
-    private void OnTriggerEnter (Collider other) {
+    // private void OnTriggerEnter (Collider other) {
         
-        if (!triggers.Contains (other))
-            triggers.Add (other);
+    //     if (!triggers.Contains (other))
+    //         triggers.Add (other);
 
-    }
+    // }
 
-    private void OnTriggerExit (Collider other) {
+    // private void OnTriggerExit (Collider other) {
         
-        if (triggers.Contains (other))
-            triggers.Remove (other);
+    //     if (triggers.Contains (other))
+    //         triggers.Remove (other);
 
-    }
+    // }
 
     public bool IsUnderwater () {
         
-        foreach (Collider trigger in triggers) {
+        // foreach (Collider trigger in triggers) {
 
-            if (trigger.GetComponentInParent<Water> ())
-                return true;
+        //     if (trigger.GetComponentInParent<Water> ())
+        //         return true;
 
-        }
+        // }
 
         return false;
 
