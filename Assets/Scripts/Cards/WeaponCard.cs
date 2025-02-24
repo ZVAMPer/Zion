27c13950a72e5875523c220f5d00ac9bf74dea11
Cooper Yang <ty1037@nyu.edu>
2025-02-24 04:00:10 -0800

    dONT KNOW IF IT WILL WORK

--- Assets/Scripts/Cards/WeaponCard.cs
+++ Assets/Scripts/Cards/WeaponCard.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b8c0bc2dd06f5fe69414d85f90852e06f7f0fc0d9d7e2eb6d6a1053e02f19aa
-size 1402
+oid sha256:aa694af0605be07cf53cea4d7118c6adcdc058865e76a7f436c11a4d437894ce
+size 1312
