1f3db88d3d75a0dcf85b2089fff7aa914c715cd7
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-02-23 17:27:35 -0800

    UI and Gameplay changes

--- Assets/Scripts/Weapons/BulletProjectile.cs
+++ Assets/Scripts/Weapons/BulletProjectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78c55c6d5f02b8c5f0238bb25b14e0c8543fb830d0367ab53d9a10c3280cc961
-size 3727
+oid sha256:ac37d08e1a6f85b4e2aeef45be996872b21f3ff1b024acd47f5eab596933cdcb
+size 4392
--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08646b16e8742501252145a82a9b894395ee92a51982dad489f832efedfda423
-size 5894
+oid sha256:7d38286f732d478ef3060c725e66afe825e17d14a4dd9e65ba0430b1652f5bc7
+size 6862
