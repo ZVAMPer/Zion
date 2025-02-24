5088a0df8f99ac505d7b0e1c9faa2dcf1c4710e5
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-02-24 07:02:24 -0800

    Add debug msg

--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:654eb2ce2e9adbf1d3d34fc989a2cabb9ff14000d93011b14119d6e71dede845
-size 7881
+oid sha256:fd950f0b00c65b20c1c4dbd1a0852cabf20ca9f30ffdb49c7a18678dbaece815
+size 7951
