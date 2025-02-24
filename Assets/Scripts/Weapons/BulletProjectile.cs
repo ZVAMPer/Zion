0ad08aba839617d5b0058d85755bd1b1c83b4b79
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-02-24 03:07:20 -0800

    Add particle effect

--- Assets/Scripts/Weapons/BulletProjectile.cs
+++ Assets/Scripts/Weapons/BulletProjectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91e0f6850a97378fd2e62226b2d263c442f5f33964e847596c310772b44eb826
-size 4738
+oid sha256:fbea067bd433efac239e7530b52a7d4ece0971b8bc56a0c1c3f05d63f973a3f8
+size 5561
