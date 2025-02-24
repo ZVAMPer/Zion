a3dd7d4e3295116f539cbf4abae7c7fda06e219d
Cooper Yang <ty1037@nyu.edu>
2025-02-24 04:23:06 -0800

    Try again

--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9d71bdd35077bd86deef50e338fd55ad004095d3ab00524232ac241f69e2d14
-size 7580
+oid sha256:2020eb3542f64ce5a3006d093eb7cc8cc14f511f5de1574d170e0f4608c7489d
+size 7787
