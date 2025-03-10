156020b9f9e15cd8e25b222d02e764a6b0f6c1d9
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-03-10 07:23:44 -0700

    Fix health don’t display issue

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a491b0426f2e284f8cabef3c14fa9deb1f662807797d372a10e380e5ca900b6
-size 3354
+oid sha256:d1cd256a9efe7b2f7d5a64df04253d0993cd31adfa1052f507f82ccd743e4855
+size 4902
