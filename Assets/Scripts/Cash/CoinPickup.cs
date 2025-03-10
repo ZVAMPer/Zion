ab53545a2a1e0e2af778f9712d6cb1a9884728c8
Cooper Yang <ty1037@nyu.edu>
2025-03-10 01:58:51 -0700

    [System] Added Coin Pick up and wallet.
    Updated project name and logo also

---
+++ Assets/Scripts/Cash/CoinPickup.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0951c83af352a34181891953f20ac574b2dd09bce57813c0626df8f771ec77f7
+size 581
---
+++ Assets/Scripts/Cash/PickUp.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:46ba7abb6cc1b46d6286292552e0afcd865d3bd6c01244eb659ff232c50795fd
+size 2681
