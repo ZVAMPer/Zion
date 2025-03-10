c4330a447b198e187bc05a46370bb5913c5ca7aa
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-03-10 07:55:02 -0700

    Hitmarker fix

--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7114e70b16b037fce3d3bfe0b8bebe3387f87c8df5eafa442d1674d0323c2aba
-size 9988
+oid sha256:85b233d0fc7d819e6509e957f6b5cbd289ae34df8a1c36102497555a86459feb
+size 12119
