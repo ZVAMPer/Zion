b0ff68e1e3d2280696c74e022b89bb8755729376
Cooper Yang <ty1037@nyu.edu>
2025-03-31 01:06:55 -0700

    [Level Art] Added KB3D Arts

---
+++ Assets/Editor/KB3DMaterialAutoCreator.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:690bb9b50987e4b89d2c48ec024030197d2cc779b26b5bdd3199a36f959259cf
+size 18352
---
+++ Assets/Editor/MaterialAutoCreator.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7d83cefe524161251ea0e23ade511d8dcaeac0ec003c408d3daa4056b0b6f4f
+size 9966
