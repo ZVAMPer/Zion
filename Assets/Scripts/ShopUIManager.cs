37530a201f5353aff044f445519324016f554b91
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-04-03 00:22:37 -0700

    Refresh passive card effect

--- Assets/Scripts/ShopUIManager.cs
+++ Assets/Scripts/ShopUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58c9899681e9676f64fb4ef19cb38d344c8dfb5548c434be5b5b336bc4da2e2b
-size 30632
+oid sha256:b35a014fd0d97d0db1d91af20cd1f500be41adac19a0504ffb29b9b3b6b7f81b
+size 30791
