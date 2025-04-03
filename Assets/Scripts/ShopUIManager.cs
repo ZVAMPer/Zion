b7564036fbf90c0a0ffc26914d95772acad0ebd5
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-04-03 00:08:26 -0700

    UI Fix

--- Assets/Scripts/ShopUIManager.cs
+++ Assets/Scripts/ShopUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5fbdad59e4dbb21fc1caa2848928b9870ae4963045fdcc9b55400cd70300970
-size 21417
+oid sha256:58c9899681e9676f64fb4ef19cb38d344c8dfb5548c434be5b5b336bc4da2e2b
+size 30632
