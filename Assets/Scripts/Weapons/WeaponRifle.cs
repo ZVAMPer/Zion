e0e27f19d01cf30563439ceb4d06e981e3e4eaf1
Cooper Yang <ty1037@nyu.edu>
2025-04-09 21:07:45 -0700

    [Bug] Fixing the client side shooting and recoil

--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67e1d34a0094ae4f5dbc9e6d143e2ed54af676c71e0803a5d434d306e7136aeb
-size 12534
+oid sha256:1e3fcefdb87ceb2509e757dcc2875e16050407951c99bdcdade69139c2644a73
+size 12905
