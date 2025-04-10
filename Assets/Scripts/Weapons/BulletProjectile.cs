f42afc139a9d27b0c4c7b98d0cb1cbac46a767e7
Cooper Yang <ty1037@nyu.edu>
2025-04-09 18:07:20 -0700

    [Bug] Bullet Hit registration fix
    visual still kinda bugged

--- Assets/Scripts/Weapons/BulletProjectile.cs
+++ Assets/Scripts/Weapons/BulletProjectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80d4f435f1f332f702598f00ff1bd678498b7fd98ad03165bc654a058cd1242c
-size 4372
+oid sha256:39bc0983330a46a69966290e51386defc22be0f6728553dcdbd7b0ee0cd714c8
+size 4321
--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85b233d0fc7d819e6509e957f6b5cbd289ae34df8a1c36102497555a86459feb
-size 12119
+oid sha256:67e1d34a0094ae4f5dbc9e6d143e2ed54af676c71e0803a5d434d306e7136aeb
+size 12534
