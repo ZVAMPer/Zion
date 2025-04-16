d3ba4d10af004fac3469ea97c02cdd712d7164b3
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-04-16 15:32:28 -0700

    Improve shop ui

--- Assets/Scripts/ShopUIManager.cs
+++ Assets/Scripts/ShopUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bb8561eef0eb2f6654b267fc5d39b8f6f94feec988bc542283a74fb673cc23d
-size 30054
+oid sha256:760ba6866fb5d72596dee0c5a0a94689391fe31ad63fa85fdce5dd99c7dd9b69
+size 30047
