fcbb0ff6a7d96b7812c9d7aaaa3f8fde5325ebd2
Cooper Yang <ty1037@nyu.edu>
2025-04-17 06:56:03 -0700

    [Audio] Improve stabiliy(maybe)

--- Assets/Scripts/Movement/SurfCharacter.cs
+++ Assets/Scripts/Movement/SurfCharacter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0366e3857d9cab987c56e4efb8247dbfd804b5a207274ec7542cc78bd2e112a
-size 39280
+oid sha256:5951b3b8a2d1943dee03bf663a994da61fe120f40e6e1d8c8783a9dd3834a705
+size 38884
