78743cce8775cc80beb908cfbf71ccae2be8e334
Cooper Yang <ty1037@nyu.edu>
2025-04-17 01:16:18 -0700

    [Audio] Default Listener bug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:678e4a681eb45e96a08f36a75d63ee6b232ba658a2617d8805ea877a96d11d83
-size 48455
+oid sha256:ad2b5d85d50b25fbad8c6aab048df0fbe279d7fbc055e0d1581e7d71cfc38d2e
+size 50108
--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ca87b07fc319b871b2ad82c094e9c26fffb0bc227078e6f5730cc2d4821a1d2
-size 7349
+oid sha256:718eb1e6a501083b781a8d4864ed47bee9a49501c3aa4cd73517abb5e4a7c159
+size 7665
