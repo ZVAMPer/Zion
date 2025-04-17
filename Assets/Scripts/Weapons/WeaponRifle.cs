19005fe717fdf80a87544376f4a6468568415523
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-04-17 05:02:17 -0700

    Shorty and shop UI adjustment

--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9d0fd5fb196a155b8da06c9842156ade05c37b573b22ee92d2c0481582f56f0
-size 12171
+oid sha256:cf04d46985ae1d4461f90f80c4e118b739a2431873a8d10eb1ec534c32f71e93
+size 12609
