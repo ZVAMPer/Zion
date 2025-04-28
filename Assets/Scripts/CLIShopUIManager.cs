7e428991bea9938b515abed1a30de4e5b2484807
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-04-28 15:33:16 -0700

    CLI Shop UI

--- Assets/Scripts/CLIShopUIManager.cs
+++ Assets/Scripts/CLIShopUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ebfe05f6ddc60a5a5a7c2c6a2ee54a2530208c904125f8a964581fc8c20da1a
-size 38350
+oid sha256:a2a32326734c2a12008f656b1fa31629d467a2e39844ef90c784d701f3438716
+size 26800
--- Assets/Scripts/Relay/LobbyManager.cs
+++ Assets/Scripts/Relay/LobbyManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6a1b70dcb35c678a0bec9d6ad2dead89f9922d82637e8de6756b74ec705a690
-size 18930
+oid sha256:95b75a5446b106709ceb24db63a590e63cc535c05f9a8c1176559a7db0b42573
+size 19512
