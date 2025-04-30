e44c807f59dba7a742b65948c92c2200e6d78418
Cooper <ty1037@nyu.edu>
2025-04-29 20:23:46 -0700

    [UI] Improve the dash cooldown ui

--- Assets/Scripts/WeaponUIManager.cs
+++ Assets/Scripts/WeaponUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91b5bd34fc4a9d49d7e70cac30695cf6031859099c2ff2052bfbd3b893bda2c2
-size 964
+oid sha256:e1cce8383ff79c66e9072cb2a3f1d9b5fa11008e697cbd371931384f181625f1
+size 1228
--- Assets/Scripts/Weapons/WeaponRifle.cs
+++ Assets/Scripts/Weapons/WeaponRifle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea8c60e45dc34e55e4d3cc4283252ab0c1cf226406eed48eab9c7bb0e83e80aa
-size 17540
+oid sha256:d5675821ff0185146d709d55beb8195666713cc080e90588daee249368e35bc5
+size 17539
