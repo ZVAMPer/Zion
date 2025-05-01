7fc9f6f71c4f608d732cc820cf61629c964341c6
Cooper Yang <ty1037@nyu.edu>
2025-04-30 22:21:50 -0700

    [audio] one more try

--- Assets/Scripts/Relay/LobbyManager.cs
+++ Assets/Scripts/Relay/LobbyManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecda321e1fcc64a7e5a023b9fa67bc1f177718f347565ef818e3a01f40f2b144
-size 19667
+oid sha256:725be0e94eb635bd0a308a66a772e8c67e743e5ee20d4e78e5df3d145136efdb
+size 19888
--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2fd1d79cb7835a0045708cf779256a5ab05162528e1714d8bff33c4ae4fbbf6
-size 10461
+oid sha256:4a2b718e0a2ae2818ee80cbfcdc3d7bb6a48324732de8650aa5b5b18a7050748
+size 11968
