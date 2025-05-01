1da75ce2b1e51b906f8fc1b457b32064fac7a3b5
Cooper Yang <ty1037@nyu.edu>
2025-04-30 23:16:48 -0700

    f

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5316e8254e287b38c477e9d6028367e0a38848d8eead6977299f0350cad3e234
-size 12693
+oid sha256:824d50e5d63e1fbf2008e5bf1b38491c486eec93f2cfb946db502f07f989b173
+size 12977
