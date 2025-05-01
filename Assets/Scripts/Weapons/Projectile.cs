3e2fa01b1c14fd77969831347412b4d72dd39bbf
Cooper Yang <ty1037@nyu.edu>
2025-04-30 23:20:37 -0700

    im retarded

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:824d50e5d63e1fbf2008e5bf1b38491c486eec93f2cfb946db502f07f989b173
-size 12977
+oid sha256:25215a59bf2c564cc9462facf30231e3b5fb7efb4fd82320992fa74fbb7c4e4e
+size 12962
