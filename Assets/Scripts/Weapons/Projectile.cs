4215543bb8b271f70380b563d0189f25a42ecd11
Cooper <ty1037@nyu.edu>
2025-04-30 17:22:00 -0700

    [Audio] Bullet Sounds

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d8cb7b700f4ea3818b4322e1a23511139fafa2845238fa4404fb1b8fbf0af1a
-size 9385
+oid sha256:2b725bc2f052bf0eabfc39f593a748573cbfa0873d241549e5beae32c52dedad
+size 9956
