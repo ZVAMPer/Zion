412fb6e13f1c81071fbd9391fda5741d37dc19be
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-05-01 00:58:38 -0700

    Compelte freeze isolation

--- Assets/Scripts/Relay/LobbyManager.cs
+++ Assets/Scripts/Relay/LobbyManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3de5e39811010c47ce09f562b2986c87e35f5478dbdbd1b32f55b8e41e1eebd6
-size 20558
+oid sha256:7ec64d31730a63111a1fe1f0505291c63bb38eb102eae624cc263ce5c7e0593b
+size 20619
--- Assets/Scripts/TUIShopManager.cs
+++ Assets/Scripts/TUIShopManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ae27598c001eab6d91969b599e98fc9babd733b842ed7b75575abb889d0ce6
-size 47793
+oid sha256:ea659815ec930e13ed9663ecb4622dbb54b811024e4616ff5fe9ce7e0de8f002
+size 48152
