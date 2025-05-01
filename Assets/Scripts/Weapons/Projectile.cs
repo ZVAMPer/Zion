bee93a8473db3b3e107f7b841e6e7049fcb0a7be
Cooper Yang <ty1037@nyu.edu>
2025-04-30 22:36:15 -0700

    bullet hate 2

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7112bf9ffb2e95e8ba0c718c054249f6e77bce39d99787ffb92214d003e60dca
-size 11799
+oid sha256:50b083eefe6af2ae829464ac533cd81621e853d160d2ae5be55b93f8f80d1b7b
+size 11974
