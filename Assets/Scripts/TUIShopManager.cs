020b91f14a1009f5f0dd398735c460d0e6f7f738
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-04-30 22:34:25 -0700

    Integrate the new TUIShoperMananger

--- Assets/Scripts/TUIShopManager.cs
+++ Assets/Scripts/TUIShopManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5011a420591f3b577cb95e35a2fa39f4357b4c22376213ba675b7dcffea9ebb9
-size 47323
+oid sha256:0933a13b4b692e302403847b0fdd0e4b9fa2a748eba57a39539342f0fb020668
+size 47319
